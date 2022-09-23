47259ece2d0c7310a46aa94598f18561fbf2e09f
Alex <89677719+alex-hatch@users.noreply.github.com>
2022-09-22 19:14:15 -0700

    Add files via upload

--- SchoolSearch.java
+++ SchoolSearch.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SchoolSearch {
 
@@ -11,10 +12,68 @@ public class SchoolSearch {
         String line = br.readLine();
         while(line != null) {
             String[] lineSplit = line.split(",");
-            Student s = new Student(lineSplit[0], lineSplit[1], Integer.parseInt(lineSplit[2]), Integer.parseInt(lineSplit[3]), Double.parseDouble(lineSplit[4]), lineSplit[5], lineSplit[6]);
+            Student s = new Student(lineSplit[0], lineSplit[1], Integer.parseInt(lineSplit[2]), Integer.parseInt(lineSplit[3]), Integer.parseInt(lineSplit[4]), lineSplit[5], lineSplit[6], lineSplit[7]);
             students.add(s);
             line = br.readLine();
         }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter in a command\n>>> ");
+
+        String command = sc.nextLine();
+
+        while(!"Quit".contains(command)) {
+            String[] commandSplit = command.split(" ");
+            if("Student".contains(commandSplit[0])) {
+                if(commandSplit.length == 3 && "Bus".contains(commandSplit[2])) {
+                    student(commandSplit[1], students, true);
+                } else {
+                    student(commandSplit[1], students, false);
+                }
+            }
+
+            System.out.print(">>> ");
+            command = sc.nextLine();
+        }
+
+    }
+
+    public static void student(String lastname, ArrayList<Student> students, boolean printBus) {
+        for(Student s : students) {
+            if(s.getStLastName().equals(lastname)) {
+                if(!printBus) {
+                    System.out.println("Last name: " + s.getStLastName());
+                    System.out.println("First name: " + s.getStFirstName());
+                    System.out.println("Grade: " + s.getGrade());
+                    System.out.println("Classroom: " + s.getClassroom());
+                } else {
+                    System.out.println("Last name: " + s.getStLastName());
+                    System.out.println("First name: " + s.getStFirstName());
+                    System.out.println("Bus route: " + s.getBus());
+                }
+
+            }
+        }
+    }
+
+    public static void teacher(String lastname, ArrayList<Student> students) {
+
+    }
+
+    public static void bus(int busNum, ArrayList<Student> students) {
+
+    }
+
+    public static void grade(int gradeNum) {
+
+    }
+
+    public static void average(int num) {
+
+    }
+
+    public static void info() {
 
     }
 
@@ -25,15 +84,17 @@ class Student {
     private final String stFirstName;
     private final int grade;
     private final int classroom;
-    private final double gpa;
+    private final int bus;
+    private final String gpa;
     private final String tLastName;
     private final String tFirstName;
 
-    public Student(String stLastName, String stFirstName, int grade, int classroom, double gpa, String tLastName, String tFirstName) {
+    public Student(String stLastName, String stFirstName, int grade, int classroom, int bus, String gpa, String tLastName, String tFirstName) {
         this.stLastName = stLastName;
         this.stFirstName = stFirstName;
         this.grade = grade;
         this.classroom = classroom;
+        this.bus = bus;
         this.gpa = gpa;
         this.tLastName = tLastName;
         this.tFirstName = tFirstName;
@@ -55,7 +116,11 @@ class Student {
         return classroom;
     }
 
-    public double getGpa() {
+    public int getBus() {
+        return bus;
+    }
+
+    public String getGpa() {
         return gpa;
     }
 
